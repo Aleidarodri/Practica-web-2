23e9e454b869cd7fa62d48724f4209fa8e3651ae
Aleidarodri <93956966+Aleidarodri@users.noreply.github.com>
2023-09-08 12:59:16 -0700

    prueba chart2

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
     colors: {
       primary: "#d19793",
       secondary: "#EFD9D1",
